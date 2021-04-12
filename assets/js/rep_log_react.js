20c613bd63734c4f88d802285dfaae59466e89b4
Vedran Milković <vedran.milkovic25@gmail.com>
2021-04-12 22:50:15 +0200

    React components render, props, state and events

--- assets/js/rep_log_react.js
+++ assets/js/rep_log_react.js
@@ -1,1 +1,10 @@
-console.log('Oh hallo React peeps!');
+import React from 'react';
+import { render } from 'react-dom';
+import RepLogApp from './RepLog/RepLogApp';
+
+const shouldShowHeart = true;
+
+render(
+    <RepLogApp withHeart={shouldShowHeart}/>,
+    document.getElementById('lift-stuff-app')
+);
